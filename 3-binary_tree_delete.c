90a6c5e95483cf9a877ba21f4e4b049e48edd085
root <huseynisrafilov17@gmail.com>
2024-01-05 07:54:37 -0800

    switched to recursion

--- 3-binary_tree_delete.c
+++ 3-binary_tree_delete.c
@@ -6,29 +6,9 @@
  */
 void binary_tree_delete(binary_tree_t *tree)
 {
-	binary_tree_t *parent = NULL;
-
-	if (tree == NULL)
-		return;
-	while (tree->left != NULL)
-		tree = tree->left;
-	while (tree->parent != NULL)
-	{
-		parent = tree->parent;
-		if (tree->right)
-			free(tree->right);
-		free(tree);
-		tree = parent;
-	}
-	while (tree->right != NULL)
-		tree = tree->right;
-	while (tree->parent != NULL)
-	{
-		parent = tree->parent;
-		if (tree->left)
-			free(tree->left);
-		free(tree);
-		tree = parent;
-	}
+	if (tree->left)
+		binary_tree_delete(tree->left);
+	if (tree->right)
+		binary_tree_delete(tree->right);
 	free(tree);
 }
--- 6-binary_tree_preorder.c
+++ 6-binary_tree_preorder.c
@@ -7,29 +7,9 @@
  */
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
 {
-	binary_tree_t *right = NULL, *tree_t = (binary_tree_t *)tree;
-
-	if (tree_t == NULL || func == NULL)
-		return;
-	(*func)(tree_t->n);
-	while (tree_t->left != NULL)
-	{
-		tree_t = tree_t->left;
-		(*func)(tree_t->n);
-	}
-	while (tree_t->parent != NULL)
-	{
-		right = tree_t->right;
-		if (right)
-			(*func)(right->n);
-		tree_t = tree_t->parent;
-	}
-	while (tree_t->right != NULL)
-	{
-		right = tree_t->right;
-		(*func)(right->n);
-		if (right->left)
-			(*func)(right->left->n);
-		tree_t = right;
-	}
+	(*func)(tree->n);
+	if (tree->left)
+		binary_tree_preorder(tree->left, func);
+	if (tree->right)
+		binary_tree_preorder(tree->right, func);
 }
