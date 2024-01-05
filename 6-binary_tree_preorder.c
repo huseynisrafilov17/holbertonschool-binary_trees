fee89999ed7e03d2afc04170f8b08d67531d48f1
root <huseynisrafilov17@gmail.com>
2024-01-05 06:45:59 -0800

    6th

---
+++ 6-binary_tree_preorder.c
@@ -0,0 +1,35 @@
+#include "binary_trees.h"
+#include <stdlib.h>
+/**
+ * binary_tree_preorder - preorder traversal.
+ * @tree: pointer to the root of the tree.
+ * @func: function pointer.
+ */
+void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int))
+{
+	binary_tree_t *right = NULL, *tree_t = (binary_tree_t *)tree;
+
+	if (tree_t == NULL)
+		return;
+	(*func)(tree_t->n);
+	while (tree_t->left != NULL)
+	{
+		tree_t = tree_t->left;
+		(*func)(tree_t->n);
+	}
+	while (tree_t->parent != NULL)
+	{
+		right = tree_t->right;
+		if (right)
+			(*func)(right->n);
+		tree_t = tree_t->parent;
+	}
+	while (tree_t->right != NULL)
+	{
+		right = tree_t->right;
+		(*func)(right->n);
+		if (right->left)
+			(*func)((right->left)->n);
+		tree_t = right;
+	}
+}
