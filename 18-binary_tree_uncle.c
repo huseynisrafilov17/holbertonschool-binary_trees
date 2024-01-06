eacc35fef2b3f2efef0a991f1a8f8b3019d74280
root <huseynisrafilov17@gmail.com>
2024-01-06 01:15:44 -0800

    17 comment fix and 18 complete

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -1,7 +1,21 @@
 #include "binary_trees.h"
 #include <stdlib.h>
 /**
- * binary_tree_sibling - finds the sibling of a node.
+ * binary_tree_uncle - finds the uncle of a node.
+ * @node: pointer to the node.
+ * Return: pointer to the uncle or NULL.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *uncle;
+
+	if (node == NULL || node->parent == NULL)
+		return (NULL);
+	uncle = binary_tree_sibling(node->parent);
+	return (uncle);
+}
+/**
+ * binary_tree_sibling - checks if a binary tree is perfect.
  * @node: pointer to the node.
  * Return: pointer to the sibling or NULL.
  */
